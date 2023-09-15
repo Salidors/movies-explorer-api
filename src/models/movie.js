a745119d5739c189e7d3a50cc6f321c83b4762dc
Salidors <110296797+Salidors@users.noreply.github.com>
2023-09-15 19:46:44 -0300

    feat: add models

---
+++ src/models/movie.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const linkRegex = /https?:\/\/.*\.?/;
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: [
+      (v) => linkRegex.test(v),
+      (props) => `${props.value} неверная ссылка постера`,
+    ],
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: [
+      (v) => linkRegex.test(v),
+      (props) => `${props.value} неверная ссылка трейлера`,
+    ],
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: [
+      (v) => linkRegex.test(v),
+      (props) => `${props.value} неверная ссылка миниатюры`,
+    ],
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
