59f6c438a2f5c642fc0b1853d9e5761a2e89a82c
Salidors <110296797+Salidors@users.noreply.github.com>
2023-09-16 11:21:41 -0300

    refactor: server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const rateLimit = require('./configs/rate-limit');
+const helmet = require('./configs/helmet');
+
+const cors = require('./middlewares/cors');
+const { errorLogger, requestLogger } = require('./middlewares/logger');
+
+const { errors } = require('celebrate');
+const router = require('./routes');
+
+const app = express();
+app.use(requestLogger);
+app.use(rateLimit);
+app.use(helmet);
+app.use(cors);
+app.use(express.json());
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-const express = require('express');
+const app = require('./app');
+const { PORT } = require('./configs/env');
+require('./db');
 
-const rateLimit = require('./configs/rate-limit');
-const helmet = require('./configs/helmet');
-
-const cors = require('./middlewares/cors');
-const { errorLogger, requestLogger } = require('./middlewares/logger');
-
-const { errors } = require('celebrate');
-const mongoose = require('mongoose');
-const router = require('./routes');
-
-const app = express();
-app.use(requestLogger);
-app.use(rateLimit);
-app.use(helmet);
-app.use(cors);
-app.use(express.json());
-app.use(router);
-app.use(errorLogger);
-app.use(errors());
-
-const port = 3000;
-
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
-
-module.exports = app;
