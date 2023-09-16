f4569d9dd149e5c10734197c5fc0cd495e582d31
Salidors <110296797+Salidors@users.noreply.github.com>
2023-09-16 10:59:24 -0300

    fix: cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,12 +1,10 @@
 const cors = require('cors');
 
-module.exports = cors(
-  cors({
-    origin: [
-      'https://api.arcana.nomoredomainsicu.ru',
-      'https://arcana.nomoredomainsicu.ru',
-      'http://localhost:3000',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  }),
-);
+module.exports = cors({
+  origin: [
+    'https://api.arcana.nomoredomainsicu.ru',
+    'https://arcana.nomoredomainsicu.ru',
+    'http://localhost:3000',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+});
